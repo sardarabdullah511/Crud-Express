cceaf53dc7555df7ccd1f07134a0eed99befd77c
Sardar Abdullah <sardarabdullah511@gmail.com>
2023-10-21 23:31:04 +0500

    new comit

--- view/backup.js
+++ view/backup.js
@@ -78,3 +78,98 @@ app.use(errorMiddleware);
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const jsonfile = require('jsonfile');
+const moment = require('moment');
+const simpleGit = require('simple-git');
+import random from 'random';
+
+const File_PATH = './data.json';
+
+
+const makeComit = n => {
+    if (n===0) return simpleGit().push();
+    const x = random.int(0,54);
+    const y = random.int(0,5);
+const Date = moment().subtract(1,'M').add(1,'d').add(x,'w').add(y,'M');
+
+
+
+const data ={
+    date:Date
+}
+console.log(Date)
+
+
+
+
+
+jsonfile.writeFile(File_PATH, data, ()=>{
+    simpleGit().add(File_PATH).commit(Date, {'--date': Date}).push();
+makeComit.bind(this, --n) 
+} )
+}
+
+makeComit(50);
+
+
+
+
+
+
+
+
+
+const jsonfile = require('jsonfile');
+const moment = require('moment');
+const simpleGit = require('simple-git');
+
+const FILE_PATH = './data.json';
+
+// Get the current date and time
+const currentDate = moment();
+
+// Subtract 30 days from the current date
+const last30Days = currentDate.subtract(30, 'days');
+
+// Format the result as a string
+const formattedDate = last30Days.format();
+
+const data = {
+    date: formattedDate
+}
+
+jsonfile.writeFile(FILE_PATH, data, (err) => {
+    if (err) {
+        console.error('Error writing JSON file:', err);
+    } else {
+        console.log('JSON file written successfully');
+
+        // Commit and push changes to Git
+        simpleGit()
+            .add(FILE_PATH)
+            .commit(formattedDate, { '--date': formattedDate })
+            .push((err, update) => {
+                if (err) {
+                    console.error('Error pushing changes to Git:', err);
+                } else {
+                    console.log('Changes pushed to Git:', update);
+                }
+            });
+    }
+});
